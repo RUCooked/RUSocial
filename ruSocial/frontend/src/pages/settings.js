fbd21429acba7baf9455cd1a7584cd9993154f96
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-12-07 18:50:27 -0500

    better formatted frontend

---
+++ ruSocial/frontend/src/pages/settings.js
@@ -0,0 +1,13 @@
+// im heartbroken and i miss my baby
+
+function Settings() {
+    return (
+      <div>
+        <h1>Welcome to My Settings</h1>
+        <p>This is where amazing things happen!</p>
+      </div>
+    );
+  }
+  
+  export default Settings;
+  
