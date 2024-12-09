59c8ecaedd12c924a85142962ccd5a87b91ef3f4
HaiderAbdelrahman <pickgamers721@gmail.com>
2024-12-09 10:19:32 -0500

    some changes to app js

--- ruSocial/frontend/src/App.js
+++ ruSocial/frontend/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home.js';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* Add more routes here for other pages */}
-        </Routes>
-      </div>
-    </Router>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Router>
+          <div className="App">
+            <button onClick={signOut}>Sign Out</button>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* Add more routes here */}
+            </Routes>
+          </div>
+        </Router>
+      )}
+    </Authenticator>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
