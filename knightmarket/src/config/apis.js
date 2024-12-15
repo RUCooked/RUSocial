47422145e070d6db6c2580610378acb859114b86
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-12-15 02:10:40 -0500

    a lot of the profile page structure but missing key functions

--- knightmarket/src/config/apis.js
+++ knightmarket/src/config/apis.js
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
   IMAGE_UPLOAD: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/images',
-  USERS: '',
+  USERS: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/user',
   MARKETPLACE_POSTS: '',
   FORUM_POSTS: '',
   CONVERSATIONS: '',
