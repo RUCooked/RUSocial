c91224c6df81bc34fde47ab5e6cc7b60b39454ed
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-12-15 16:11:52 -0500

    corrected API endpoints

--- knightmarket/src/config/apis.js
+++ knightmarket/src/config/apis.js
@@ -1,8 +1,9 @@
 export const API_ENDPOINTS = {
   IMAGE_UPLOAD: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/images',
   USERS: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/user',
-  MARKETPLACE: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/marketplace',
-  FORUM: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/forum',
+  MARKETPLACE_POSTS: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/marketplace',
+  MARKETPLACE_GET: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/marketplace',
+  FORUM_POSTS: '',
   CONVERSATIONS: '',
   MESSAGES: ''
 };
