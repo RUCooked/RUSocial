a690af2fc1d00ffc4139f8325e99a184aafeafa1
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-12-15 14:48:38 -0500

    correctly retrieves user posts and post counts

--- knightmarket/src/config/apis.js
+++ knightmarket/src/config/apis.js
@@ -1,9 +1,8 @@
 export const API_ENDPOINTS = {
   IMAGE_UPLOAD: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/images',
   USERS: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/user',
-  MARKETPLACE_POSTS: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/marketplace',
-  MARKETPLACE_GET: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/marketplace',
-  FORUM_POSTS: '',
+  MARKETPLACE: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/marketplace',
+  FORUM: 'https://r0s9cmfju1.execute-api.us-east-2.amazonaws.com/cognito-testing/forum',
   CONVERSATIONS: '',
   MESSAGES: ''
 };
